db9f76925a04bb9a3cd030b202c03d70ac1238c9
Bruce Kim <mannypac12@gmail.com>
2018-12-24 20:54:22 +0900

    Redux Thunk Done

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,12 +1,16 @@
 import * as actionTypes from './actionTypes' 
 
+
+
 export const saveResult = (res) => {
     return {type: actionTypes.STORE_RESULT, result: res};
 }
 
 export const storeResult= (res) => {
-    return dispatch => {
+    return (dispatch, getState) => {
         setTimeout(() => {
+            const oldCounter = getState().ctr.counter;
+            console.log(oldCounter)
             dispatch(saveResult(res))
         }, 2000)
     }
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,30 +1,34 @@
 import * as actionTypes from '../actions/actionTypes';
+import {
+    updateObject
+} from '../utility'
 
 const initialState = {
     counter: 0
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
+            return updateObject(state, {
+                counter: state.counter + 1
+            })
+
         case actionTypes.DECREMENT:
-            return {
-                ...state,
+            return updateObject(state, {
                 counter: state.counter - 1
-            }
+            })
+
         case actionTypes.ADD:
-            return {
-                ...state,
+            return updateObject(state, {
                 counter: state.counter + action.val
-            }
+            })
+
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
+            return updateObject(state, {
                 counter: state.counter - action.val
-            }
+            })
     }
     return state;
 };
